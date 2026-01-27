81d450a062d56b5eaae18e9969c8c1b9e564a9ed
Adeka <Adekaimanjanov@gmail.com>
2026-01-27 22:41:04 +0500

    sql databas connection fix

--- src/main/java/com/company/data/PostgresDB.java
+++ src/main/java/com/company/data/PostgresDB.java
@@ -5,13 +5,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class PostgresDB implements IDB {
-    // поменяй под себя
+
     private static final String URL = "jdbc:postgresql://localhost:5432/trading_db";
     private static final String USER = "postgres";
     private static final String PASS = "0000";
 
     @Override
     public Connection getConnection() throws Exception {
+        Class.forName("org.postgresql.Driver");
         return DriverManager.getConnection(URL, USER, PASS);
     }
 }
