d805e87f9d5dab8d7643843be69c76230fe6d379
Adeka <Adekaimanjanov@gmail.com>
2026-02-03 22:09:08 +0500

    Little fixes

--- src/main/java/com/company/MyApplication.java
+++ src/main/java/com/company/MyApplication.java
@@ -1,10 +1,12 @@
 package com.company;
+
 import com.company.controllers.TradeController;
 import com.company.data.PostgresDB;
 import com.company.data.interfaces.IDB;
 import com.company.repositories.AssetRepository;
 import com.company.repositories.TradeRepository;
 import com.company.repositories.UserRepository;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -12,12 +14,16 @@ import java.util.Scanner;
 public class MyApplication {
     public void start() {
         System.out.println("Application started...");
+
         Scanner scanner = new Scanner(System.in);
         IDB db = new PostgresDB();
+
         AssetRepository assetRepo = new AssetRepository(db);
         UserRepository userRepo = new UserRepository(db);
         TradeRepository tradeRepo = new TradeRepository(db);
+
         TradeController controller = new TradeController(assetRepo, userRepo, tradeRepo, scanner);
+
         Map<String, Runnable> actions = new HashMap<>();
         actions.put("1", () -> controller.listAssets());
         actions.put("2", () -> controller.addAsset());
@@ -25,6 +31,11 @@ public class MyApplication {
         actions.put("4", () -> controller.buy());
         actions.put("5", () -> controller.sell());
         actions.put("6", () -> controller.portfolio());
+
+
+        actions.put("7", () -> controller.banUser());
+        actions.put("8", () -> controller.tradeHistory());
+
         while (true) {
             System.out.println("\n1) List assets");
             System.out.println("2) Add asset");
@@ -32,10 +43,14 @@ public class MyApplication {
             System.out.println("4) Buy");
             System.out.println("5) Sell");
             System.out.println("6) Portfolio");
+            System.out.println("7) Ban user");
+            System.out.println("8) Trade history");
             System.out.println("0) Exit");
             System.out.print("> ");
+
             String c = scanner.nextLine();
             if ("0".equals(c)) break;
+
             Runnable action = actions.get(c);
             if (action != null) {
                 action.run();
