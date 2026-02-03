5613209671b9c7a47f4a97f0238d5d627c5ff97c
ts_ni <madzoid26@gmail.com>
2026-02-03 21:04:07 +0500

    IUserRepository update

--- src/main/java/com/company/repositories/interfaces/IUserRepository.java
+++ src/main/java/com/company/repositories/interfaces/IUserRepository.java
@@ -1,12 +1,12 @@
 package com.company.repositories.interfaces;
 
-import com.company.models.Role;
 import com.company.models.User;
 
 public interface IUserRepository {
+
     boolean create(User u);
     User getById(int id);
     User getByName(String name);
-    boolean updateRole(int userId, Role newRole);
+    boolean updateRole(int userId, String newRole);
     boolean setBanStatus(int userId, boolean banned);
 }
